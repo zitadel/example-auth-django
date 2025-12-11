14a539759fd7044735e7284b394989fdefda834f
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-12-11 12:39:53 +1100

    feat: initial commit

--- project/jinja2.py
+++ project/jinja2.py
@@ -6,7 +6,8 @@ from jinja2 import Environment
 
 def environment(**options: Any) -> Environment:
     """Configure Jinja2 environment with Django URL resolver."""
-    env = Environment(autoescape=True, **options)
+    options.setdefault("autoescape", True)
+    env = Environment(**options)  # noqa: S701
     env.globals.update(
         {
             "url_for": lambda route_name, **kwargs: reverse(route_name, kwargs=kwargs),
